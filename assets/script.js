5ee82dbf0a5afaa317b463e27485a013dc75d0ae
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-20 22:53:52 -0400

    made form functional with formspree.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
+  $("#submitButton").click(function (event) {
+    event.preventDefault();
+    console.log("clicked button");
+
+    const name = $("#inputName").val();
+    const email = $("#inputEmail").val();
+    const textArea = $("#messageTextArea").val();
+    const statusEl = $(".status");
+    statusEl.empty()
+
+    if (name.length > 2) {
+        statusEl.append("<div>Name is Valid</div>")
+    } else {
+        event.preventDefault();
+        statusEl.append("<div>Name is NOT Valid</div>")
+    }
+
+    if (email.length > 5 && email.includes("@") && email.includes(".")) {
+        statusEl.append("<div>Email is Valid</div>")
+    } else {
+        event.preventDefault();
+        statusEl.append("<div>Email is NOT Valid</div>")
+    }
+
+    if (textArea.length > 1) {
+        statusEl.append("<div>Message is Valid</div>")
+    } else {
+        event.preventDefault();
+        statusEl.append("<div>Message is NOT Valid</div>")
+    }
     
+  });
 });
